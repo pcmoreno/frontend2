d2dfef7296dad83ee4ad2118358528a6c990545b
Robbin van Ooij <robbin@vanooij.nl>
2017-12-13 13:59:29 +0100

    halfway implementing add-item logic. retrieve initial items on component load

--- src/js/pages/ExamplePage/actions/example.js
+++ src/js/pages/ExamplePage/actions/example.js
@@ -8,3 +8,19 @@ export function addRandomItem(itemId) {
         item: itemId
     };
 }
+
+export function addItem(item) {
+    // return action type and the value(s) to be sent to reducer for state mutation
+
+    return {
+        type: actionType.ADD_ITEM,
+        item
+    };
+}
+
+export function getItems(items) {
+    return {
+        type: actionType.GET_ITEMS,
+        items
+    };
+}
