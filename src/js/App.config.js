02c2e1d8db758d96f2cef959bd81cfe5ffbd26dd
Sander Sikkema <s.sikkema@ltp.nl>
2018-03-05 14:39:29 +0100

    simplify app structure and removed unnecessary objects

--- src/js/App.config.js
+++ src/js/App.config.js
@@ -4,23 +4,15 @@
  * @type {{}}
  */
 module.exports = {
-    components: {
-
-    },
-    modules: {
-
-    },
-    utils: {
-        api: {
-            neon: {
-                baseUrl: 'http://dev.ltponline.com:8001/api/v1',
-                endpoints: {
-                    organisations: '/section/organisation'
-                },
-                urlEncodeParams: false,
-                skipPrefixIndexParams: true,
-                requestFailedMessage: 'An error occurred while processing your request.'
-            }
+    api: {
+        neon: {
+            baseUrl: 'http://dev.ltponline.com:8001/api/v1',
+            endpoints: {
+                organisation: '/section/organisation'
+            },
+            urlEncodeParams: false,
+            skipPrefixIndexParams: true,
+            requestFailedMessage: 'An error occurred while processing your request.'
         }
     }
 };
