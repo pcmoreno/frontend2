f20f7d7da090075ee302828ddd339beeb30348fc
Robbin van Ooij <robbin@vanooij.nl>
2018-03-22 14:33:33 +0100

    ensure service workers only works on https

--- src/js/utils/common.js
+++ src/js/utils/common.js
@@ -1,31 +1,31 @@
 // use common.js for functions that need to be executed on page load
 
 // performs feature query for navigator, then initialises service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js').then(() => {
-            // console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            // note: if storage was NOT cleared (or caching=false), service worker install event wont be triggered
-        }, (err) => {
-            // registration failed
-            console.log('ServiceWorker registration failed: ', err);
-        }).catch((err) => {
-            console.log(err);
+if(location.protocol === 'https:'){
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('service-worker.js').then(() => {
+                // console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                // note: if storage was NOT cleared (or caching=false), service worker install event wont be triggered
+            }, (err) => {
+                // registration failed
+                console.log('ServiceWorker registration failed: ', err);
+            }).catch((err) => {
+                console.log(err);
+            });
         });
-    });
 
-    window.addEventListener('online', () => {
-        document.querySelector('body').classList.remove('offline');
-    }, false);
+        window.addEventListener('online', () => {
+            document.querySelector('body').classList.remove('offline');
+        }, false);
 
-    window.addEventListener('offline', () => {
-        document.querySelector('body').classList.add('offline');
-    }, false);
-} else {
-    console.log('serviceWorker not supported on this device, cant work offline');
+        window.addEventListener('offline', () => {
+            document.querySelector('body').classList.add('offline');
+        }, false);
+    } else {
+        console.log('serviceWorker not supported on this device, cant work offline');
+    }
 }
-
-
 
 // typekit
 (function(d) {
