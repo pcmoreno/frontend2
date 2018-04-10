2fdcc84b62db6fda9baaab106dffded28ebe96e7
Robbin van Ooij <robbin@vanooij.nl>
2018-04-10 16:10:00 +0200

    commented out things that are not used yet

--- src/js/pages/Users/components/Users/Users.js
+++ src/js/pages/Users/components/Users/Users.js
@@ -4,9 +4,10 @@ import { h, Component } from 'preact';
 
 import Listview from '../../../../components/Listview';
 import Sidebar from './../../../../components/Sidebar';
-import Report from './components/Report/Report';
-import Participant from './components/Participant/Participant';
-import Project from './components/Project/Project';
+
+// import Report from './components/Report/Report';
+// import Participant from './components/Participant/Participant';
+// import Project from './components/Project/Project';
 import style from './style/users.scss';
 
 export default class Users extends Component {
