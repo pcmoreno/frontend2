0dcc1caef156c004f8a6e4de0197b349f786ab3d
Robbin van Ooij <robbin@vanooij.nl>
2018-05-02 09:44:09 +0200

    re-add missing inbox translations

---
+++ data/i18n/inbox-nl_NL.js
@@ -0,0 +1,13 @@
+export default {
+    "complete_before": "afronden voor",
+    "inbox": "inbox",
+    "invitation": "uitnodiging",
+    "invitation_description_text": "U bent uitgenodigd voor een assessment. Wanneer u op start klikt, gaat u naar het testprogramma. Elk onderdeel van het programma is voorzien van een instructie en tijdsindicatie. U kunt tussen de onderdelen door pauzeren.",
+    "log_out": "uitloggen",
+    "organisation": "organisatie",
+    "organisations": "organisaties",
+    "participants": "deelnemers",
+    "start": "start",
+    "tasks": "taken",
+    "users": "gebruikers"
+};
