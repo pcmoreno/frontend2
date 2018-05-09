4319ddec291309cbc5f4df056ac3720f8f86107c
Robbin van Ooij <robbin@vanooij.nl>
2018-05-09 16:26:51 +0200

    remove useless transformation in reducer

--- src/js/pages/Organisations/reducers/organisations.js
+++ src/js/pages/Organisations/reducers/organisations.js
@@ -191,11 +191,9 @@ export default function organisationsReducer(state = initialState, action) {
             });
 
             // now add the new form taken from the action
-            // todo: remove uuid example of supplying the form with a default value (be it hidden or not)
             newForm = {
                 id: action.formId,
-                formFields: action.formFields,
-                uuid: 'something'
+                formFields: action.formFields
             };
 
             newState.forms.push(newForm);
