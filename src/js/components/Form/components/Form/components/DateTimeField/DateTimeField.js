0a66d814b904cca4759592f88fd2a2a3903b91b9
Robbin van Ooij <robbin@vanooij.nl>
2018-06-16 13:07:10 +0200

    include flatpickr

--- src/js/components/Form/components/Form/components/DateTimeField/DateTimeField.js
+++ src/js/components/Form/components/Form/components/DateTimeField/DateTimeField.js
@@ -4,13 +4,28 @@ import { h, Component } from 'preact';
 
 import style from '../style/field.scss';
 import Utils from '../../../../../../utils/utils';
+import flatpickr from 'flatpickr';
 
 export default class DateTimeField extends Component {
 
+    componentDidMount() {
+
+        const id = `#${this.props.formId}_${this.props.handle}`;
+
+        // init flatpickr
+        flatpickr(
+            id,
+            {
+                enableTime: true,
+                time_24hr: true,
+                defaultHour: 9,
+                defaultMinute: 0
+            }
+        );
+    }
+
     render() {
         const { currentForm, handle, label, onChange, value, formId } = this.props;
-
-        // todo: need a datetime field (preact extension) that can handle 24 hour clock and timezones
 
         return (
             <div>
@@ -21,11 +36,13 @@ export default class DateTimeField extends Component {
                     </li>
                     <li>
                         <input
-                            type={ 'datetime-local' }
+                            type="text"
+                            placeholder="Select Date.."
+                            readOnly="readonly"
+                            onChange={ onChange }
+                            name={ handle }
                             id={ `${formId}_${handle}` }
                             value={ Utils.formatDate(value, 'yyyy-MM-ddThh:mm:ss') || '' }
-                            name={ handle }
-                            onChange={ onChange }
                         />
                     </li>
                 </ul>
