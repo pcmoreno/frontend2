4f987dfc38444a9aa98f5cd4a7894ce8ba8376c9
Robbin van Ooij <robbin@vanooij.nl>
2018-06-25 13:37:34 +0200

    remove consolelog

--- src/js/pages/Organisations/reducers/organisations.js
+++ src/js/pages/Organisations/reducers/organisations.js
@@ -216,8 +216,6 @@ export default function organisationsReducer(state = initialState, action) {
 
         case actionType.STORE_FORM_DATA:
 
-            console.table(action.formFields)
-
             // clear all forms
             newState.forms = [];
 
