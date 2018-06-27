7a8a3fed5b80d6400ce756274a7c1ef37f46db59
Robbin van Ooij <robbin@vanooij.nl>
2018-06-27 14:31:43 +0200

    small fix

--- src/js/pages/Inbox/components/Inbox/Inbox.js
+++ src/js/pages/Inbox/components/Inbox/Inbox.js
@@ -32,6 +32,25 @@ export default class Inbox extends Component {
                             </section>
                         </div>
                     </li>
+                    <li>
+                        <div className={ style.message }>
+                            <article className={ style.participantsSummary }>
+                                <h3>Bekijk jouw Persona.fit</h3>
+                                <p>De Persona.fit resultaten zijn gebaseerd op de vragenlijst die je hebt ingevuld.
+                                    Klik op 'Ga naar Persona.fit' om jouw resultaten te zien.</p>
+                            </article>
+                            <section className={ style.participantsData }>
+                                <h4>LTP</h4>
+                                <div>
+                                    <div className={ style.participantsAction }>
+                                        <a className="action_button" href="#notimplemented">
+                                            Ga naar Persona.fit
+                                        </a>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </li>
                 </ul>
             </main>
         );
