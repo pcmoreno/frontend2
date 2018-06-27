99cac9d9fcb10a53e589238869b19af899432516
Robbin van Ooij <robbin@vanooij.nl>
2018-06-27 14:32:20 +0200

    remove mock

--- src/js/pages/Inbox/components/Inbox/Inbox.js
+++ src/js/pages/Inbox/components/Inbox/Inbox.js
@@ -11,48 +11,7 @@ export default class Inbox extends Component {
 
     render() {
         return (
-            <main className={ style.inbox }>
-                <ul className={ style.messages }>
-                    <li>
-                        <div className={ style.message }>
-                            <article className={ style.participantsSummary }>
-                                <h3>Bekijk jouw Persona.fit</h3>
-                                <p>De Persona.fit resultaten zijn gebaseerd op de vragenlijst die je hebt ingevuld.
-                                    Klik op 'Ga naar Persona.fit' om jouw resultaten te zien.</p>
-                            </article>
-                            <section className={ style.participantsData }>
-                                <h4>LTP</h4>
-                                <div>
-                                    <div className={ style.participantsAction }>
-                                        <a className="action_button" href="#notimplemented">
-                                            Ga naar Persona.fit
-                                        </a>
-                                    </div>
-                                </div>
-                            </section>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={ style.message }>
-                            <article className={ style.participantsSummary }>
-                                <h3>Bekijk jouw Persona.fit</h3>
-                                <p>De Persona.fit resultaten zijn gebaseerd op de vragenlijst die je hebt ingevuld.
-                                    Klik op 'Ga naar Persona.fit' om jouw resultaten te zien.</p>
-                            </article>
-                            <section className={ style.participantsData }>
-                                <h4>LTP</h4>
-                                <div>
-                                    <div className={ style.participantsAction }>
-                                        <a className="action_button" href="#notimplemented">
-                                            Ga naar Persona.fit
-                                        </a>
-                                    </div>
-                                </div>
-                            </section>
-                        </div>
-                    </li>
-                </ul>
-            </main>
+            <main className={ style.inbox } />
         );
     }
 }
