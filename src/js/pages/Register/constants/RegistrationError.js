05dec011641b6761e62a5edb5cf33c2ad305de96
Sander Sikkema <s.sikkema@ltp.nl>
2018-06-29 13:34:47 +0200

    Add registration error constants

---
+++ src/js/pages/Register/constants/RegistrationError.js
@@ -0,0 +1,12 @@
+/**
+ * These are used as exception messages for the registration component
+ * be aware that these keys are also mapped to lokalise to load the translations
+ */
+const RegistrationError = {
+    ALL_FIELDS_REQUIRED: 'register_all_fields_required',
+    PASSWORD_DONT_MATCH: 'register_passwords_dont_match',
+    LOGIN_FAILED: 'register_login_failed',
+    UNEXPECTED_ERROR: 'api_general_error'
+};
+
+export default RegistrationError;
