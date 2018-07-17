d5aabf257c8d63ff55d7feb493b11ff75ac68902
Sander Sikkema <sander@MacBook-Pro-van-Sander.local>
2018-07-17 15:29:27 +0200

    implement field error translations and fallback messages

---
+++ src/js/components/Form/constants/FormErrors.js
@@ -0,0 +1,7 @@
+const FormErrors = {
+    COULD_NOT_PROCESS_REQUEST: 'form_could_not_process_your_request',
+    VALUE_CAN_NOT_BE_EMPTY: 'form_value_can_not_be_empty',
+    INVALID_FIELD_VALUE: 'form_error_invalid_field_value'
+};
+
+export default FormErrors;
