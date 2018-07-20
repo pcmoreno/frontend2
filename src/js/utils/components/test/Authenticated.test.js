489a2e23f664bead84553ecfe30e31462f806a36
Sander Sikkema <sander@MacBook-Pro-van-Sander.local>
2018-07-20 16:39:44 +0200

    Add some tests

--- src/js/utils/components/test/Authenticated.test.js
+++ src/js/utils/components/test/Authenticated.test.js
@@ -1,3 +1,49 @@
-test('Should be implemented', () => {
-    // todo: implement tests for this module
+import { shallow } from 'preact-render-spy';
+import Authenticated from '../Authenticated';
+
+test('Should render a div when is authenticated', () => {
+    const api = {
+        getAuthenticator: () => {
+            return {
+                isAuthenticated: () => {
+                    return true;
+                }
+            };
+        }
+    };
+
+    const context = shallow(<Authenticated api={api}>
+        <div>Some authenticated component</div>
+    </Authenticated>);
+
+    expect(context.find('div')[0].children).toEqual([
+        {
+            "attributes": undefined,
+            "children": [
+                "Some authenticated component"
+            ],
+            "key": undefined,
+            "nodeName": "div"
+        }
+    ]);
+});
+
+test('Should not render a div when is not authenticated', () => {
+    const api = {
+        getAuthenticator: () => {
+            return {
+                isAuthenticated: () => {
+                    return false;
+                }
+            };
+        }
+    };
+
+    const context = shallow(<Authenticated api={api}>
+        <div>Some authenticated component</div>
+    </Authenticated>);
+
+    expect(context.find('div')).toEqual({
+        "length": 0
+    });
 });
