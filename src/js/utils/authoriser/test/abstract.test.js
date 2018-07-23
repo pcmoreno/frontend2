a5f3de141364e3ed189b2ee7854347bb8e6b1706
Sander Sikkema <sander@MacBook-Pro-van-Sander.local>
2018-07-23 12:57:16 +0200

    extend Authoriser tests

--- src/js/utils/authoriser/test/abstract.test.js
+++ src/js/utils/authoriser/test/abstract.test.js
@@ -3,15 +3,19 @@ import AbstractAuthoriser from '../abstract';
 // below seems unused, but is required for jest to mock/overwrite it
 import AppConfig from '../../../App.config';
 
-// Mock the application api config
+// Mock the application authorize config
 jest.mock('../../../App.config', () => {
     return {
         authoriser: {
             neon: {
-                x: 'y'
-            },
-            fake: {
-
+                x: 'y',
+                report: {
+                    writeAction: [
+                        'Administrator',
+                        'Manager'
+                    ]
+                },
+                loginRedirect: '/login'
             }
         }
     }
@@ -31,8 +35,55 @@ test('AbstractAuthoriser should throw an error when the authoriser config is not
     }
 });
 
+test('Authorise should return true on matching correct roles', () => {
+    const authoriser = new AbstractAuthoriser('neon');
+    const user = {
+        getRoles: () => {
+            return [
+                'administrator'
+            ];
+        }
+    };
+
+    spyOn(authoriser, 'getAllowedRolesForComponentAction').and.callThrough();
+
+    const authorised = authoriser.authorise(user, 'report', 'writeAction');
+
+    expect(authorised).toBe(true);
+    expect(authoriser.getAllowedRolesForComponentAction.calls.count()).toBe(1);
+    expect(authoriser.getAllowedRolesForComponentAction.calls.allArgs()).toEqual([
+        [
+            'report',
+            'writeAction'
+        ]
+    ]);
+});
+
+test('Authorise should return false on not matching correct roles', () => {
+    const authoriser = new AbstractAuthoriser('neon');
+    const user = {
+        getRoles: () => {
+            return [
+                'cleaner'
+            ];
+        }
+    };
+
+    spyOn(authoriser, 'getAllowedRolesForComponentAction').and.callThrough();
+
+
+    const authorised = authoriser.authorise(user, 'report', 'writeAction');
+
+    expect(authorised).toBe(false);
+    expect(authoriser.getAllowedRolesForComponentAction.calls.count()).toBe(1);
+    expect(authoriser.getAllowedRolesForComponentAction.calls.allArgs()).toEqual([
+        [
+            'report',
+            'writeAction'
+        ]
+    ]);
+});
+
 // todo: add unit test for getComponentConfig
 
 // todo: add unit test for getAllowedRolesForComponentAction
-
-// todo: add unit test for authorize
