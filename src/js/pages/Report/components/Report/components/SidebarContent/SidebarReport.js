0ade9034fa57a50c322b7c0280e25650e736862e
Sander Sikkema <sander@MacBook-Pro-van-Sander.local>
2018-08-03 09:14:26 +0200

    input type and range checking for static scores

--- src/js/pages/Report/components/Report/components/SidebarContent/SidebarReport.js
+++ src/js/pages/Report/components/Report/components/SidebarContent/SidebarReport.js
@@ -6,7 +6,25 @@ import StaticScoreValue from '../../../../constants/StaticScoreValue';
 
 /** @jsx h */
 
+const sidebarScorePrefix = 'sidebar_score_';
+const staticScoreRegexRange = `${StaticScoreValue.MIN_VALUE}-${StaticScoreValue.MAX_VALUE}`;
+const staticScoreFullRegex = new RegExp(`[^${staticScoreRegexRange}]`, 'g');
+
 export default class SidebarReport extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onInputStaticScore = this.onInputStaticScore.bind(this);
+    }
+
+    onInputStaticScore(event) {
+        const value = event.target.value.replace(staticScoreFullRegex, '').slice(0, 1);
+
+        if (value !== event.target.value) {
+            event.target.value = value;
+        }
+    }
 
     render() {
         const { i18n, reportTexts, staticScores } = this.props;
@@ -36,7 +54,15 @@ export default class SidebarReport extends Component {
                 staticScoreRows.push(
                     <tr>
                         <td>{ i18n[`report_${Utils.camelCaseToSnakeCase(key)}`] || key}</td>
-                        <td><input type='number' min={StaticScoreValue.MIN_VALUE} max={StaticScoreValue.MAX_VALUE} value={score || ''} /></td>
+                        <td><input
+                            id={ `${sidebarScorePrefix}${key}` }
+                            onInput={ this.onInputStaticScore }
+                            type='number'
+                            pattern={ `[${staticScoreRegexRange}]` }
+                            min={ StaticScoreValue.MIN_VALUE }
+                            max={ StaticScoreValue.MAX_VALUE }
+                            value={ score || '' }
+                        /></td>
                     </tr>
                 );
             });
