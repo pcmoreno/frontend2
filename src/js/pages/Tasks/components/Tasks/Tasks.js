2e475fc515f25b1ab0566253e835eaf9528b4094
Robbin van Ooij <robbin@vanooij.nl>
2018-09-13 12:22:33 +0200

    show indicator to load complete overview on Tasks page

--- src/js/pages/Tasks/components/Tasks/Tasks.js
+++ src/js/pages/Tasks/components/Tasks/Tasks.js
@@ -6,18 +6,39 @@ import Listview from '../../../../components/Listview';
 import style from './style/tasks.scss';
 
 export default class Tasks extends Component {
+    constructor(props) {
+        super(props);
+
+        this.showAllTasks = this.showAllTasksHandler.bind(this);
+    }
+
+    showAllTasksHandler() {
+        this.props.showAllTasks();
+    }
+
     render() {
-        const { tasks, i18n } = this.props;
+        const { tasks, i18n, showAllTasksFlag } = this.props;
 
         return (
-            <main className={ style.tasks } id="tasks">
-                <Listview
-                    entities={ tasks }
-                    defaultSortingKey={ 'assessmentdate' }
-                    defaultSortingOrder={ 'desc' }
-                    i18n={i18n}
-                    translationKeyPrefix={ 'tasks_' }
-                />
+            <main className={ `${showAllTasksFlag ? 'fullheight' : ''}` } id="tasks">
+                <section className={ style.listview_container } >
+                    <Listview
+                        entities={ tasks }
+                        defaultSortingKey={ 'assessmentdate' }
+                        defaultSortingOrder={ 'desc' }
+                        i18n={i18n}
+                        translationKeyPrefix={ 'tasks_' }
+                    />
+                </section>
+                <div
+                    className={ `${style.show_all_tasks} ${showAllTasksFlag ? 'hidden' : 'inline'}` }
+                    onClick={ () => this.showAllTasksHandler() }
+                    role="button"
+                    tabIndex="0"
+                >
+                    <span>{ i18n.tasks_overview_only_shows_most_recent_entries } </span>
+                    { i18n.tasks_overview_click_to_show_all_participants }
+                </div>
             </main>
         );
     }
