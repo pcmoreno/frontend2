3eb2cec4df4db2796bac8bb2aca010fdc4a6e4bb
Paulo Moreno <p.moreno@ltp.nl>
2018-10-29 11:33:58 +0000

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -1,10 +1,12 @@
 import I18n from 'react-native-i18n';
 
 import en_GB from './data/i18/report-en_GB.js'
+import nl_NL from './data/i18n/report-nl_NL.js'
 
 I18n.fallbacks = true;
 
 I18n.translations = {
+    nl_NL,
     en_GB
 };
 
